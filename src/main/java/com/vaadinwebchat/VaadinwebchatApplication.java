21e259eda766a616b6ce1137f7065f3dc75348b7
marksalch <marksirius96@gmai.com>
2022-08-06 01:53:59 +0800

    Initial commit

---
+++ src/main/java/com/vaadinwebchat/VaadinwebchatApplication.java
@@ -0,0 +1,13 @@
+package com.vaadinwebchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinwebchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinwebchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaadinwebchat/VaadinwebchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaadinwebchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinwebchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
