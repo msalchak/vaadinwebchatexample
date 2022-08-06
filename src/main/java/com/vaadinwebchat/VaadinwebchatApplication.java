79fd91f085fc29da251865337ee19a8cd7f922ff
marksalch <marksirius96@gmai.com>
2022-08-06 23:14:16 +0800

    done

--- src/main/java/com/vaadinwebchat/VaadinwebchatApplication.java
+++ src/main/java/com/vaadinwebchat/VaadinwebchatApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VaadinwebchatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VaadinwebchatApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinwebchatApplication.class, args);
+    }
 
 }
--- src/test/java/com/vaadinwebchat/VaadinwebchatApplicationTests.java
+++ src/test/java/com/vaadinwebchat/VaadinwebchatApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class VaadinwebchatApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
